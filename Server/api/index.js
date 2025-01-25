e96dd5972b0573a7a5db50d59d7555146a92caf4
thebhaveshshinde23 <thebhaveshshinde@gmail.com>
2025-01-25 09:29:17 +0530

    Endpoint

--- Server/api/index.js
+++ Server/api/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
         origin:"https://real-time-chatting-app-mu.vercel.app",
-        methods:["GET","POST"],
+        method:["GET","POST"],
     }
 })
 
