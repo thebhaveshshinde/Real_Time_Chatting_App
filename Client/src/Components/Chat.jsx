b040dc2fdbed51eab1092ac99f2167c6b2e30540
thebhaveshshinde23 <thebhaveshshinde@gmail.com>
2025-01-25 09:20:40 +0530

    Deployment Setup for Frontend

--- Client/src/Components/Chat.jsx
+++ Client/src/Components/Chat.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { io } from "socket.io-client"
-const socket = io("http://localhost:4000")
+const socket = io("https://real-time-chat-app-back.vercel.app")
 
 
 function Chat() {
