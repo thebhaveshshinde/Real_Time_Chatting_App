9b737b8c34ad4afba189bfb7f5f6675bff62c07e
thebhaveshshinde <thebhaveshshinde@gmail.com>
2025-01-26 13:12:01 +0530

    Update Chat.jsx

--- Client/src/Components/Chat.jsx
+++ Client/src/Components/Chat.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { io } from "socket.io-client"
-const socket = io("https://real-time-chat-app-back.vercel.app",{
+const socket = io("https://real-time-chat-app-back.vercel.app/api",{
   transports: ['polling'],
 });
 
